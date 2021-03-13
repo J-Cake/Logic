173359ba37bd34f29aa02cf497bd4158ff1e235d
jake - server <jakieschneider13@gmail.com>
2021-03-13 17:09:40 +1100

    added optional Data dir creating

--- bin/configuredb.js
+++ bin/configuredb.js
@@ -5,6 +5,9 @@ import sqlite3 from 'sqlite3';
 
 const dir = path.dirname(import.meta.url.slice(6));
 
+if (!fs.existsSync('./Data'))
+	fs.mkdirSync('./Data');
+
 const db = new (sqlite3.verbose().Database)('./Data/Users');
 
 db.serialize(function() {
