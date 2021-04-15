e64c12a58fc9a7d6fafbdf0b08277eb8ed3ddcc2
JCake <jakieschneider13@gmail.com>
2021-04-16 00:47:09 +1000

    Added update signal

--- srv/HTTP/AdminRouter.ts
+++ srv/HTTP/AdminRouter.ts
@@ -4,6 +4,8 @@ const router: express.Router = express.Router();
 
 router.post('/update-docs', async function (req, res) {
     console.log("--- Unimplemented --- [Updating `lib` is not implemented yet]");
+    res.status(200);
+    res.end('')
 });
 
 export default router;
